635b30daa2106e5448dd8c4ff9a41e9e009960b5
Kevin Lloyd Bernal <kevinoxy@gmail.com>
2022-08-22 16:05:02 +0800

    mark mypy errors in tests as xfail

--- tests/test_mypy.py
+++ tests/test_mypy.py
@@ -34,85 +34,154 @@ URL = "https://www.some-page/"
 RESPONSE = HttpResponse(URL, b"")
 
 
+# NOTES: The following test cases below for assigning ``ResponseUrl`` instances
+# are marked with "xfail" due to the following known mypy bugs:
+#   - https://github.com/python/mypy/issues/10187
+#   - https://github.com/python/mypy/issues/5313
+#   - https://github.com/python-attrs/attrs/issues/889
+# Currently, the said bugs causes mypy to raise the following error:
+#   'Incompatible types in assignment (expression has type "ResponseUrl",
+#   variable has type "Optional[str]")'
+# FIXME: Unmark these from "xfail" once the upstream mypy issues are solved.
+
+
+@pytest.mark.mypy_testing
+@pytest.mark.xfail
+def test_assignment_image_url():
+    img = Image(url=RESPONSE.url)
+    img.url = RESPONSE.url
+
+
+@pytest.mark.mypy_testing
+@pytest.mark.xfail
+def test_assignment_breadcrumb_url():
+    bc = Breadcrumb(url=RESPONSE.url)
+    bc.url = RESPONSE.url
+
+
+@pytest.mark.mypy_testing
+@pytest.mark.xfail
+def test_assignment_link_url():
+    link = Link(url=RESPONSE.url)
+    link.url = RESPONSE.url
+
+
+@pytest.mark.mypy_testing
+@pytest.mark.xfail
+def test_assignment_product_url():
+    product_1 = Product(url=RESPONSE.url)
+    product_1.url = RESPONSE.url
+
+    product_2 = Product(url=RESPONSE.url)
+    product_2.canonicalUrl = RESPONSE.url
+
+
+@pytest.mark.mypy_testing
+@pytest.mark.xfail
+def test_assignment_product_list_url():
+    product_list_1 = ProductList(url=RESPONSE.url)
+    product_list_1.url = RESPONSE.url
+
+    product_list_2 = ProductList(url=RESPONSE.url)
+    product_list_2.canonicalUrl = RESPONSE.url
+
+
+@pytest.mark.mypy_testing
+@pytest.mark.xfail
+def test_assignment_product_from_list_url():
+    product_from_list_1 = ProductFromList(url=RESPONSE.url)
+    product_from_list_1.url = RESPONSE.url
+
+
+@pytest.mark.mypy_testing
+@pytest.mark.xfail
+def test_assignment_product_variant_url():
+    product_variant_1 = ProductVariant(url=RESPONSE.url)
+    product_variant_1.url = RESPONSE.url
+
+    product_variant_2 = ProductVariant(url=RESPONSE.url)
+    product_variant_2.canonicalUrl = RESPONSE.url
+
+
 @pytest.mark.mypy_testing
 def test_assignment_image():
     img_1 = Image(url=RESPONSE.url)
-    img_1.url = RESPONSE.url  # E: Incompatible types in assignment (expression has type "ResponseUrl", variable has type "str")
+    img_1.url = str(RESPONSE.url)
 
     img_2 = Image(url=RESPONSE.url)
-    img_2.url = str(RESPONSE.url)
+    img_2.url = "https://www.example.com"
 
 
 @pytest.mark.mypy_testing
 def test_assignment_breadcrumb():
     bc_1 = Breadcrumb(url=RESPONSE.url)
-    bc_1.url = RESPONSE.url  # E: Incompatible types in assignment (expression has type "ResponseUrl", variable has type "Optional[str]")
+    bc_1.url = str(RESPONSE.url)
 
     bc_2 = Breadcrumb(url=RESPONSE.url)
-    bc_2.url = str(RESPONSE.url)
+    bc_2.url = "https://www.example.com"
 
 
 @pytest.mark.mypy_testing
 def test_assignment_link():
     link_1 = Link(url=RESPONSE.url)
-    link_1.url = RESPONSE.url  # E: Incompatible types in assignment (expression has type "ResponseUrl", variable has type "Optional[str]")
+    link_1.url = str(RESPONSE.url)
 
     link_2 = Link(url=RESPONSE.url)
-    link_2.url = str(RESPONSE.url)
+    link_2.url = "https://www.example.com"
 
 
 @pytest.mark.mypy_testing
 def test_assignment_product():
     product_1 = Product(url=RESPONSE.url)
-    product_1.url = RESPONSE.url  # E: Incompatible types in assignment (expression has type "ResponseUrl", variable has type "str")
+    product_1.url = str(RESPONSE.url)
 
     product_2 = Product(url=RESPONSE.url)
-    product_2.url = str(RESPONSE.url)
+    product_2.url = "https://www.example.com"
 
     product_3 = Product(url=RESPONSE.url)
-    product_3.canonicalUrl = RESPONSE.url  # E: Incompatible types in assignment (expression has type "ResponseUrl", variable has type "Optional[str]")
+    product_3.canonicalUrl = str(RESPONSE.url)
 
     product_4 = Product(url=RESPONSE.url)
-    product_4.canonicalUrl = str(RESPONSE.url)
+    product_4.canonicalUrl = "https://www.example.com"
 
 
 @pytest.mark.mypy_testing
 def test_assignment_product_list():
     product_list_1 = ProductList(url=RESPONSE.url)
-    product_list_1.url = RESPONSE.url  # E: Incompatible types in assignment (expression has type "ResponseUrl", variable has type "str")
+    product_list_1.url = str(RESPONSE.url)
 
     product_list_2 = ProductList(url=RESPONSE.url)
-    product_list_2.url = str(RESPONSE.url)
+    product_list_2.url = "https://www.example.com"
 
     product_list_3 = ProductList(url=RESPONSE.url)
-    product_list_3.canonicalUrl = RESPONSE.url  # E: Incompatible types in assignment (expression has type "ResponseUrl", variable has type "Optional[str]")
+    product_list_3.canonicalUrl = str(RESPONSE.url)
 
     product_list_4 = ProductList(url=RESPONSE.url)
-    product_list_4.canonicalUrl = str(RESPONSE.url)
+    product_list_4.canonicalUrl = "https://www.example.com"
 
 
 @pytest.mark.mypy_testing
 def test_assignment_product_from_list():
     product_from_list_1 = ProductFromList(url=RESPONSE.url)
-    product_from_list_1.url = RESPONSE.url  # E: Incompatible types in assignment (expression has type "ResponseUrl", variable has type "Optional[str]")
+    product_from_list_1.url = str(RESPONSE.url)
 
     product_from_list_2 = ProductFromList(url=RESPONSE.url)
-    product_from_list_2.url = str(RESPONSE.url)
+    product_from_list_2.url = "https://www.example.com"
 
 
 @pytest.mark.mypy_testing
 def test_assignment_product_variant():
     product_variant_1 = ProductVariant(url=RESPONSE.url)
-    product_variant_1.url = RESPONSE.url  # E: Incompatible types in assignment (expression has type "ResponseUrl", variable has type "Optional[str]")
+    product_variant_1.url = str(RESPONSE.url)
 
     product_variant_2 = ProductVariant(url=RESPONSE.url)
-    product_variant_2.url = str(RESPONSE.url)
+    product_variant_2.url = "https://www.example.com"
 
     product_variant_3 = ProductVariant(url=RESPONSE.url)
-    product_variant_3.canonicalUrl = RESPONSE.url  # E: Incompatible types in assignment (expression has type "ResponseUrl", variable has type "Optional[str]")
+    product_variant_3.canonicalUrl = str(RESPONSE.url)
 
     product_variant_4 = ProductVariant(url=RESPONSE.url)
-    product_variant_4.canonicalUrl = str(RESPONSE.url)
+    product_variant_4.canonicalUrl = "https://www.example.com"
 
 
 @pytest.mark.mypy_testing
