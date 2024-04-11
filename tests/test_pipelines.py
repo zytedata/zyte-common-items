d10102fddd9e5281089d90932caada39fa63d856
PyExplorer <stgmont@gmail.com>
2024-04-11 21:28:06 +0300

    fix issue B009 Do not call getattr...

--- tests/test_pipelines.py
+++ tests/test_pipelines.py
@@ -15,7 +15,9 @@ from zyte_common_items.pipelines import DropItem, DropLowProbabilityItemPipeline
         ("Article", {"Product": 0.2}, 0.01, 0.01),
     ],
 )
-def test_get_threshold(settings, item_class_name, default_threshold, expected_threshold):
+def test_get_threshold(
+    settings, item_class_name, default_threshold, expected_threshold
+):
     spider = MagicMock()
     spider.settings.get.return_value = settings
     zyte_common_items.pipelines.DEFAULT_ITEM_PROBABILITY_THRESHOLD = default_threshold
@@ -27,13 +29,36 @@ def test_get_threshold(settings, item_class_name, default_threshold, expected_th
     assert threshold == expected_threshold
 
 
-@pytest.mark.parametrize("item, item_proba, threshold, expected_stats_calls, expected_return", [
-    (MagicMock(), None, 0.1, ["item/crawl/total", "item/crawl/extracted_with_high_proba"], True),
-    (MagicMock(), 0.5, 0.1, ["item/crawl/total", "item/crawl/extracted_with_high_proba"], True),
-    (MagicMock(), 0.01, 0.1, ["item/crawl/total", "item/crawl/dropped_with_low_proba"], None),
-])
+@pytest.mark.parametrize(
+    "item, item_proba, threshold, expected_stats_calls, expected_return",
+    [
+        (
+            MagicMock(),
+            None,
+            0.1,
+            ["item/crawl/total", "item/crawl/extracted_with_high_proba"],
+            True,
+        ),
+        (
+            MagicMock(),
+            0.5,
+            0.1,
+            ["item/crawl/total", "item/crawl/extracted_with_high_proba"],
+            True,
+        ),
+        (
+            MagicMock(),
+            0.01,
+            0.1,
+            ["item/crawl/total", "item/crawl/dropped_with_low_proba"],
+            None,
+        ),
+    ],
+)
 @pytest.mark.asyncio
-async def test_process_item(item, item_proba, threshold, expected_stats_calls, expected_return):
+async def test_process_item(
+    item, item_proba, threshold, expected_stats_calls, expected_return
+):
     spider = MagicMock()
     stats = MagicMock()
 
@@ -46,10 +71,12 @@ async def test_process_item(item, item_proba, threshold, expected_stats_calls, e
     try:
         returned_item = await pipeline.process_item(item, spider)
     except DropItem as e:
-        assert (f"The item: {item!r} is dropped as the probability ({item_proba}) is "
-                f"below the threshold ({threshold})") in str(e)
+        assert (
+            f"The item: {item!r} is dropped as the probability ({item_proba}) is "
+            f"below the threshold ({threshold})"
+        ) in str(e)
     else:
         assert returned_item == item
 
     for call in expected_stats_calls:
-        getattr(stats, "inc_value").assert_any_call(call, spider=spider)
+        stats.inc_value.assert_any_call(call, spider=spider)
