28d62a5187b17e712af5ab0df54ce83d01fcf3d6
Andrey Rakhmatullin <wrar@wrar.name>
2025-08-22 13:55:26 +0500

    Mark test_get_type_hints() as xfail on older web-poet.

--- tests/test_annotations.py
+++ tests/test_annotations.py
@@ -1,6 +1,8 @@
+from importlib.metadata import version
 from typing import get_type_hints
 
 import pytest
+from packaging.version import Version
 
 import zyte_common_items
 
@@ -12,6 +14,10 @@ EXPORTED = [
 EXPORTED_TYPES = [t for t in EXPORTED if isinstance(t, type)]
 
 
+@pytest.mark.xfail(
+    Version(version("web-poet")) < Version("0.19.2"),
+    reason="Older web-poet has some broken annotations",
+)
 @pytest.mark.parametrize("t", EXPORTED_TYPES)
 def test_get_type_hints(t: type) -> None:
     """Test that get_type_hints() works for all exported types."""
